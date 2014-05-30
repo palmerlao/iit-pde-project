e2815eb4f7dfb562cd44a6bfcd27d9d7b7f8f2ab
palmer <palmer@palmer.palmer>
2014-05-30 12:40:08 -0400

    Fixed BC problem, collocation using translated kernel basis is done (I hope). Started working on implementing Newton basis

--- code/colloc_1dbvp.m
+++ code/colloc_1dbvp.m
@@ -2,32 +2,64 @@ clear all; close all; clc
 
 % Solves u'' = 1 + e^(2x), u(0) = 0 = u(1)
 
-epsilon = 50;
+epsilon = 350;
 f   = @(x,center) ( exp(-epsilon.*((x-center).^2)) );
 D1f = @(x,center) ( -2.*epsilon.*(x-center).*f(x,center) );
 D2f = @(x,center) ( 2.*epsilon.*(2.*epsilon.*((x-center).^2)-1).* ...
                     f(x,center) );
 
+
 rhs = @(x) ( 1 + exp(2.*x) );
+u_analytic = @(x) ( 0.25.*((2.*x.^2)-exp(2).*x-x+exp(2.*x)-1) );
 
-hold on;
 pts = linspace(0,1);
-colors = 'rgkyc';
-for N=[4 8 16 23];
-    colloc_pts = linspace(0,1,N);
-    tmp = repmat(colloc_pts, N, 1);
-    colloc_mat = D2f(tmp',tmp);
-    coef = colloc_mat\[0;rhs(colloc_pts(2:end-1))';0];
+subplot(1,2,1);
+hold on;
+colors = 'grcmyk';
 
-    u_numeric = @(x) ( f(x,colloc_pts)*coef );
+%% Calculate numerical solution
+for N=[4 8 12 16 20 24];
+    colloc_pts = linspace(0,1,N);
+    
+    % translated basis
+    tmp = repmat(colloc_pts, N, 1);
+    colloc_mat = [D2f(tmp',tmp)   zeros(N,2); 
+                  f(colloc_pts,0) 1 0;
+                  f(colloc_pts,1) 1 1];
+    coef = colloc_mat\[rhs(colloc_pts)';0;0]; % include 0 bc
+    
+    % newton basis
+    lambda = zeros(N,1);
+    for i=2:N;
+        lambda(i) = rhs(colloc_pts(i)) - lambda(i-1);
+    end
+    
+
+    u_numeric = @(x) ( [f(x,colloc_pts) 1 x]*coef );
     plot(pts, arrayfun(u_numeric, pts), colors(1));
     colors = colors(2:end);
 end
-u_analytic = @(x) ( 0.25.*((2.*pts.^2)-exp(2).*pts-pts+exp(2.*pts)-1) );
+
 plot(pts, u_analytic(pts));
-title('Solution to u'' = 1+exp(2x), u(0) = 0 = u(1)');
+title('Solution to u\prime\prime = 1+exp(2x), u(0) = 0 = u(1)');
 legend('4 elems', ...
        '8 elems', ...
+       '12 elems', ...
        '16 elems', ...
-       '23 elems', ...
+       '20 elems', ...
+       '40 elems', ...
        'analytic')
+
+%% Calculate condition numbers of collocation matrices
+trans_cond = zeros(100);
+newt_cond = zeros(100);
+subplot(1,2,2);
+for N=1:100;
+    colloc_pts = linspace(0,1,N);
+    tmp = repmat(colloc_pts, N, 1);
+    trans_cond(N) = cond(D2f(tmp',tmp));
+end
+semilogy(trans_cond);
+title('Condition Number of collocation matrices for N points');
+legend('translated basis');
+xlabel('N');
