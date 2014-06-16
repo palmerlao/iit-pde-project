a7cbb9e3aa29a629335cee79e1dbd5abddb16c4f
Tim McCollam <tmccolla@hawk.iit.edu>
2014-06-15 23:41:53 -0500

    Updated N to be exponential. Updated plots. **Make sure M (K\V) is calculated correctly! is not upper triangular**

--- code/CompareCond.m
+++ code/CompareCond.m
@@ -1,3 +1,4 @@
+
 clear all; close all; clc;
 set(0,'defaultLineLineWidth',3) %thick lines
 
@@ -5,20 +6,26 @@ set(0,'defaultLineLineWidth',3) %thick lines
 
 subplot(1, 2, 1);
 
-Nstart = 1;
-Nend = 100;
+count = 1;
+NStart = 0;
+NEnd = 7;
 
-condPlotK = zeros(1, Nend - Nstart);
-condPlotKTilda = zeros(1, Nend - Nstart);
-condPlotL = zeros(1, Nend - Nstart);
-condPlotLTilda = zeros(1, Nend - Nstart);
-condPlotV = zeros(1, Nend - Nstart);
-condPlotP = zeros(1, Nend - Nstart);
+NVector = floor(2.^(NStart:1:NEnd));
+
+condPlotK = zeros(1, NEnd - NStart);
+condPlotKTilda = zeros(1, NEnd - NStart);
+condPlotL = zeros(1, NEnd - NStart);
+condPlotLTilda = zeros(1, NEnd - NStart);
+condPlotV = zeros(1, NEnd - NStart);
+condPlotVTilda = zeros(1, NEnd - NStart);
+condPlotP = zeros(1, NEnd - NStart);
+condPlotPTilda = zeros(1, NEnd - NStart);
 f=@(x) 1+exp(2*x);
 u_analytic = @(x) ( 0.25.*((2.*x.^2)-exp(2).*x-x+exp(2.*x)-1) );
-errL=zeros(1,Nend-Nstart);
-errP=zeros(1,Nend-Nstart);
-for N = Nstart : 1 : Nend
+errL=zeros(1,NEnd-NStart);
+errP=zeros(1,NEnd-NStart);
+for N = NVector
+   %keyboard
    %fix K and take derivatives (taken from Palmer's code)
    %epsilon = 200;
    epsilon = (N/4).^2;
@@ -43,63 +50,86 @@ for N = Nstart : 1 : Nend
                K(0, samplePoints) 1 0;
                K(1, samplePoints) 1 1];
     cond(KMatrix);
-    condPlotK(N-Nstart+1) = cond(KMatrix);
-    condPlotKTilda(N-Nstart+1) = cond(KMatrixTilda);
-    condPlotL(N-Nstart+1) = cond(LMatrix);
-    condPlotLTilda(N-Nstart+1) = cond(LMatrixTilda);
+    condPlotK(count) = cond(KMatrix);
+    condPlotKTilda(count) = cond(KMatrixTilda);
+    condPlotL(count) = cond(LMatrix);
+    condPlotLTilda(count) = cond(LMatrixTilda);
     
     %keyboard
     uL=[KMatrix ones(N,1) samplePoints']*(LMatrixTilda\[fsample;0;0]);
-    errL(N-Nstart+1)=max(abs(usample-uL));
+    errL(count)=max(abs(usample-uL));
     
     
     %%Newton Basis
     [B, VMatrix] = calculate_beta_v(KMatrix, N, samplePoints, K);
-    condPlotV(N-Nstart+1) = cond(VMatrix);
+    condPlotV(count) = cond(VMatrix);
     
-    %should work since L is linear operator (double check)
+    VMatrixTilda = [VMatrix ones(N, 1) samplePoints';
+                    ones(1, N) 0 0;
+                    samplePoints 0 0];
+    
+    condPlotVTilda(count) = cond(VMatrixTilda);
+    
+    %should work since L is linear operator 
     [B, PMatrix] = calculate_beta_v(LMatrix, N, samplePoints, D2K);
-    condPlotP(N-Nstart+1) = cond(PMatrix);
+    condPlotP(count) = cond(PMatrix);
      PMatrixTilda = [PMatrix zeros(N, 2);
                VMatrix(:,1)' 1 0;
                VMatrix(:,end)' 1 1];
 
+    condPlotPTilda(count) = cond(PMatrixTilda);
     uP=[VMatrix ones(N,1) samplePoints']*(PMatrixTilda\[fsample;0;0]);
-    errP(N-Nstart+1)=max(abs(usample-uP));
+    errP(count)=max(abs(usample-uP));
+    
+    
 
-
+count = count+1;
 end
 
-    semilogy(Nstart:1:Nend, condPlotK, ':b')
+    semilogy(NVector, (condPlotK), ':b')
     hold on;
-    semilogy(Nstart:1:Nend, condPlotKTilda, 'b')
-    semilogy(Nstart:1:Nend, condPlotL, ':g')
-    semilogy(Nstart:1:Nend, condPlotLTilda, 'g')
-    semilogy(Nstart:1:Nend, condPlotV, ':r')
-    semilogy(Nstart:1:Nend, condPlotP, ':c')
+    semilogy(NVector, (condPlotKTilda), 'b')
+    semilogy(NVector, (condPlotL), ':g')
+    semilogy(NVector, (condPlotLTilda), 'g')
+    semilogy(NVector, (condPlotV), ':r')
+    semilogy(NVector, (condPlotVTilda), 'r')
+    semilogy(NVector, (condPlotP), ':c')
+    semilogy(NVector, (condPlotPTilda), 'c')
     
     title('Condition Number vs. Number of points sampled');
-    legend('K Matrix', 'K~ Matrix', 'L Matrix', 'L~ Matrix', 'V Matrix', 'P Matrix', 'Location', 'NorthWest');
+    legend('K Matrix', 'K~ Matrix', 'L Matrix', 'L~ Matrix', 'V Matrix','V~ Matrix', 'P Matrix', 'P~ Matrix', 'Location', 'NorthWest');
     ylabel('Condition Number');
     xlabel('Number of points sampled');
 
 
    subplot(1, 2, 2);
     hold on;
-    plot(Nstart:1:Nend, (condPlotK./condPlotL), ':g')
-    plot(Nstart:1:Nend, (condPlotKTilda./condPlotLTilda), 'g')
-    plot(Nstart:1:Nend, (condPlotV./condPlotP), ':r')
+    plot(NVector, (condPlotK./condPlotL), ':g')
+    plot(NVector, (condPlotKTilda./condPlotLTilda), 'g')
+    plot(NVector, (condPlotV./condPlotP), ':r')
     
     title('Ratio of Condition Numbers');
     legend('K/L', 'K~/L~', 'V/P', 'Location', 'NorthWest');
     ylabel('Ratios');
     xlabel('Number of points sampled');
     
+    %keyboard
     figure
-    semilogy(Nstart:1:Nend,errL,'g',Nstart:1:Nend,errP,'r')
+    subplot(1, 2, 1);
+    plot(samplePoints, usample, 'b', samplePoints, uL, 'g', samplePoints, uP, 'c')
+    
+    title('Analytic and Numerical functions');
+    legend('Analytic Solution', 'Kernel Basis Solution', 'Newton Basis Solution');
+    
+    subplot(1, 2, 2);
+    semilogy(NVector,errL,'g',NVector,errP,'c')
+    
+    title('Absolute Error');
+    legend('Error with Kernel Basis', 'Error with Newton Basis', 'Location', 'Northwest');
+    ylabel('Absolute Error');
+    xlabel('Number of points sampled');
 
-    %see what ratios are 
-    %KLRatio = condPlotK (100)/condPlotL(100)
-    %KLTildaRatio = condPlotKTilda (100)/condPlotLTilda(100)
-    %VPRatio = condPlotV (100)/condPlotP(100)
-    condPlotV(end)
+
+
+
+
