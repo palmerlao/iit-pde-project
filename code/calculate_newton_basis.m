d690a1d129ff24ce64fd6bb4bb13d8adbf90a5e3
palmer <palmer@palmer.palmer>
2014-06-25 15:17:57 -0400

    changed calculate_newton_basis, now v*v'-KM ~ 0 but v != tril(v) I guess this doesn't really matter and is an artifact of the adaptive point selection

--- code/calculate_newton_basis.m
+++ code/calculate_newton_basis.m
@@ -4,13 +4,14 @@ function [V] = calculate_newton_basis(KM)
     w = zeros(N,1);
     u = zeros(N,1);
 
-    % get first column of value matrix
-    V(:,1) = KM(:,1)./ sqrt(abs(KM(1,1)));
     z = diag(KM);
+    % get first column of value matrix
+    V(:,1) = KM(:,1)./ sqrt(max(abs(z)));
     w = w + V(:,1).^2;
     for i=2:N
-        V(:,i) = (KM(i,:)' - V(:,1:(i-1))*V(i,1:(i-1))') ...
-                  ./ sqrt(abs(z(i)-w(i)));
-        w = w + V(:,i).^2;    
+        [zm, zmind] = max(z-w);
+        V(:,i) = (KM(:,zmind) - V(:,1:i)*V(zmind,1:i)') ...
+                  ./ sqrt(abs(z(zmind)-w(zmind)));
+        w = w + V(:,i).^2;
     end
 end
